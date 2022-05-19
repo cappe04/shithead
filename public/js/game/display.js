7de5074531c2e866a4f7f22b1d2b0d222cdf6ec9
Goby56 <60710855+Goby56@users.noreply.github.com>
2022-05-19 18:59:42 +0200

    Hand listener on opponent

--- public/js/game/display.js
+++ public/js/game/display.js
@@ -1,18 +1,27 @@
+
+
 function initListeners() {
     const deck = database.ref("rooms/" + roomKey + "/game" + "/deck");
     const stack = database.ref("rooms/" + roomKey + "/game" + "/stack");
-    const users = await getUsers(roomKey);
+    const uids = Object.keys(getUsers(roomKey))
 
     userIterator:
-    for (let user of users) {
+    for (let uid of uids) {
         const userRef = database.ref("rooms/" + roomKey + "/users/" + user)
+        const userCardCounter = document.querySelector(`#${uid}-card-counter`)
+        
         if (user == firebase.auth().currentUser.uid) {
-
+            // Hand listeners on client
+            // Fixa på sin egen hand så rätt kort visas
+            
         } else {
+            // Hand listeners on oponents
             userRef.child("hand").on("child_added", (snapshot) => {
-
+                userCardCounter.innerHTML = userCardCounter.value + 1
             })
-
+            userRef.child("hand").on("child_removed", (snapshot) => {
+                userCardCounter.innerHTML = userCardCounter.value - 1
+            })
         }
 
         
