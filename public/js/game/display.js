76612c7c847e6ef156c5ed663789f3692a8cc365
Goby56 <60710855+Goby56@users.noreply.github.com>
2022-05-19 19:28:29 +0200

    More listeners

--- public/js/game/display.js
+++ public/js/game/display.js
@@ -5,6 +5,20 @@ function initListeners() {
     const stack = database.ref("rooms/" + roomKey + "/game" + "/stack");
     const uids = Object.keys(getUsers(roomKey))
 
+    deck.on("child_removed", (snapshot) => {
+        const deckCardCounter = document.querySelector("#deck-card-counter") 
+        deckCardCounter.innerHTML = deckCardCounter.value + 1
+    })
+
+    stack.on("child_added", (snapshot) => {
+        // Lägg till det tillagda kortet högst upp
+        console.log(snapshot.toJSON())
+    })
+
+    stack.on("child_removed", (snapshot) => {
+        // Ta bort hela högen (vända högen)
+    })
+
     userIterator:
     for (let uid of uids) {
         const userRef = database.ref("rooms/" + roomKey + "/users/" + user)
@@ -15,12 +29,24 @@ function initListeners() {
             // Fixa på sin egen hand så rätt kort visas
             
         } else {
-            // Hand listeners on oponents
+            // Hand listeners on opponents
             userRef.child("hand").on("child_added", (snapshot) => {
                 userCardCounter.innerHTML = userCardCounter.value + 1
             })
             userRef.child("hand").on("child_removed", (snapshot) => {
                 userCardCounter.innerHTML = userCardCounter.value - 1
+            })
+
+            // Visible card listeners on opponents
+            userRef.child("hidcards").on("child_removed", (snapshot) => {
+                // Ta bort rätt kort med avseende på id
+                console.log(snapshot.key)
+            })
+
+            // Hidden card listeners on opponents
+            userRef.child("hidcards").on("child_removed", (snapshot) => {
+                // Ta bort rätt kort med avseende på id
+                console.log(snapshot.key)
             })
         }
 
@@ -36,7 +62,7 @@ function UIAddUser(uid){
             <div class="playing-card" id="${uid}-card2"></div>
             <div class="playing-card" id="${uid}-card3"></div>
         </div>
-        <div id="${uid}-card-counter" class="card-counter">5</div>
+        <div id="${uid}-card-counter" class="card-counter">0</div>
     </div>`
 
     document.getElementById("opponents-container").innerHTML += html
