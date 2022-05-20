05c7ebd94c20a6a5e9f95b57ccf3ee4a0b0360d4
Casper Bené <48201144+cappe04@users.noreply.github.com>
2022-05-20 13:29:29 +0200

    remove room when leaving page

--- public/js/config.js
+++ public/js/config.js
@@ -15,4 +15,23 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-var roomKey;
+var roomKey = null;
+
+window.onbeforeunload = async function(){
+
+    if(roomKey != null){
+        const user = firebase.auth().currentUser;
+
+        let roomInfo = await getSnapshot("rooms/" + roomKey + "/room-info", (snapshot) => {
+            return snapshot.toJSON();
+        })
+
+        if(user.uid == roomInfo.owner){
+            database.ref("rooms/" + roomKey).remove()
+        } else {
+            database.ref("rooms/" + roomKey + "/users/" + user.uid).remove()
+        }
+    }
+
+}
+
